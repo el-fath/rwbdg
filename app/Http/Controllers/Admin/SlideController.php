b067da1a22f0171643f3dd83ac4c389aec21e143
el-fath <rochman003@gmail.com>
2019-08-02 14:23:21 +0700

    add slide

--- app/Http/Controllers/Admin/SlideController.php
+++ app/Http/Controllers/Admin/SlideController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Slide;
 
 class SlideController extends Controller
 {
+    public function __construct()
+    {
+        $this->img_location = "public/image/admin/";
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,8 +19,9 @@ class SlideController extends Controller
      */
     public function index()
     {
-        $title = "Slide";
-        return view("admin/slide/index",compact('title'));
+        $data['title'] = "Slide";
+        $data['action'] = route('admin.slide.store');
+        return view("admin/slide/index",compact('data'));
     }
 
     /**
@@ -36,7 +42,23 @@ class SlideController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if ($request->file('image')) {
+            $file    = $request->file('image');
+            $ext     = $file->getClientOriginalExtension();
+            $newName = rand(100000,1001238912).".".$ext;
+            $file->move($this->img_location.'slide',$newName);
+        }else{
+            $newName = NULL;
+        }
+
+        $data = [
+            'title' => $request->title,
+            'desc'  => $request->desc,
+            'image' => $newName,
+        ];
+
+        $data = Slide::create($data);
+        return $data;
     }
 
     /**
--- resources/views/admin/slide/index.blade.php
+++ resources/views/admin/slide/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin/template')
 @section('title')
-{{$title}}
+{{$data['title']}}
 @endsection
 @section('content')
 <div class="content-wrapper">
@@ -9,7 +9,7 @@
     <div class="page-header page-header-light">
         <div class="page-header-content header-elements-md-inline">
             <div class="page-title d-flex">
-                <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">Home</span> - {{$title}}
+                <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">Home</span> - {{$data['title']}}
                 </h4>
                 <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
             </div>
@@ -19,7 +19,7 @@
             <div class="d-flex">
                 <div class="breadcrumb">
                     <a href="index.html" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
-                    <span class="breadcrumb-item active">{{$title}}</span>
+                    <span class="breadcrumb-item active">{{$data['title']}}</span>
                 </div>
 
                 <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
@@ -39,7 +39,7 @@
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-header header-elements-inline">
-                            <h6 class="card-title">{{$title}}</h6>
+                            <h6 class="card-title">{{$data['title']}}</h6>
                                 <div class="header-elements">
                                     <div class="list-icons">
                                         <a class="list-icons-item" data-action="collapse"></a>
@@ -48,25 +48,26 @@
                             </div>
 
                             <div class="card-body">
-                                <form action="#">
+                                <form action="{{$data['action']}}" method="POST" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
                                     <fieldset class="mb-3">
                                         <div class="form-group row">
                                             <label class="col-form-label col-lg-2">Title</label>
                                             <div class="col-lg-10">
-                                                <input type="text" class="form-control" placeholder="title banner">
+                                                <input type="text" class="form-control" placeholder="title banner" name="title">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label class="col-form-label col-lg-2">Image</label>
                                             <div class="col-lg-10">
-                                                <input type="file" class="file-input" data-fouc>
+                                                <input type="file" class="file-input" name="image">
                                                 <span class="form-text text-muted">Automatically convert a file input to a bootstrap file input widget by setting its class as <code>file-input</code>.</span>
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label class="col-form-label col-lg-2">Description</label>
                                             <div class="col-lg-10">
-                                                <textarea rows="3" cols="3" class="form-control" placeholder="description"></textarea>
+                                                <textarea rows="3" cols="3" class="form-control" name="desc" placeholder="description"></textarea>
                                             </div>
                                         </div>
                                 </form>
